1f93ffbd015508d1c280cd73ce2b08fa3fea00c0
Kevin <185861753+Scrasms@users.noreply.github.com>
2025-08-08 11:57:28 +1000

    added truth button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react';
+import wordle_trauma from './assets/wordle-trauma.png';
+import truth_text from './assets/truth.txt?raw';
+import './App.css';
 
 function App() {
   const [time, setTime] = useState();
+  const [truth, setTruth] = useState();
 
    useEffect(() => {
       const interval = setInterval(() => {
@@ -29,10 +32,21 @@ function App() {
   return (
     <>
       <div className='home'>
+        <span className='blah'></span>
         <span className='title'> Next Wordle in: </span>
         {time ? <div className='timer'>
-          {time.hours} hours: {time.minutes} minutes: {time.seconds} seconds
+          {time.hours} hrs : {time.minutes} mins : {time.seconds} secs
         </div> : <span className='timer'>Getting your time...</span>}
+        <button onClick={() => setTruth(!truth)}>TRUTH</button>
+
+        <div className='truth'>
+          {truth ? <img className='truth-image' src={wordle_trauma}/>
+          : null}
+          {truth ? <div className='truth-text'>
+            {truth_text}
+          </div>
+          : null}
+        </div>
       </div>
     </>
   )
